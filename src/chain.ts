d777dde59eb596bb325df7fffa91fc29e6e74923
ParkDongJo <pdj10211@gmail.com>
2023-09-20 18:13:19 +0900

    Feat: lodash 예제연습 구축

---
+++ src/chain.ts
@@ -0,0 +1,21 @@
+import _ from 'lodash'
+import {IPerson, makeRandomIPerson} from './models/person'
+
+// 지연평가 테스트
+
+const persons: IPerson[] = _.range(10).map(makeRandomIPerson);
+// console.log('persons', persons);
+
+const names: string[] = _.chain(persons)
+  .filter((person) => {
+    console.log('filter', person.age)
+    return person.age >= 20 && person.age < 30
+  })
+  .map((person) => {
+    console.log('map', person.age)
+    return person.name
+  })
+  .take(2)
+  .value();
+
+console.log('names', names);
